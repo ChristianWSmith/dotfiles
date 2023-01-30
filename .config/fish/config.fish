9359de36a2d2462a1d758318dbe81d1ae3477efc
Christian Smith <smith.christian.william@gmail.com>
2023-01-30 15:09:32 -0500

    auto-launch sway on tty1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -g fish_greeting
+    if [ "$XDG_VTNR" -eq 1 ] && [ -z "$DISPLAY" ]
+        /usr/bin/sway 2>&1 >> ~/.sway.log
+    end
 end
