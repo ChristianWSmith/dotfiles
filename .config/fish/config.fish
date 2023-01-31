458fb1ecf087ca1200922590388eb84c0760e914
Christian Smith <smith.christian.william@gmail.com>
2023-01-31 14:25:11 -0500

    --no-wall shutdown/reboot

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,6 @@ if status is-interactive
     if [ "$XDG_VTNR" -eq 1 ] && [ -z "$DISPLAY" ]
         /usr/bin/sway 2>&1 > /dev/null
     end
+    alias "shutdown" "systemctl poweroff --no-wall"
+    alias "reboot" "systemctl reboot --no-wall --reboot"
 end
