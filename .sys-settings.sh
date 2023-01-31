df315f7b22f63bc267756d09ab70d3bddb8f6a05
Christian Smith <smith.christian.william@gmail.com>
2023-01-31 18:27:19 -0500

    todo in .sys-settings.sh

--- .sys-settings.sh
+++ .sys-settings.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#sudo usermod -a -G wheel $USER
-#sudo usermod -a -G seat $USER
-#sudo /bin/bash -c "rsync -a $HOME/.config/fish/ ~/.config/fish"
+sudo usermod -a -G wheel $USER
+sudo usermod -a -G seat $USER
+sudo /bin/bash -c "rsync -a $HOME/.config/fish/ ~/.config/fish"
 
+# TODO: inject literal '$TERM' into file
 autologin_file="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
-
 sudo /bin/bash -c "rm -f $autologin_file; tee -a $autologin_file <<EOF
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin $USER %I $(which $TERM)
 Environment=XDG_SESSION_TYPE=wayland
 EOF" > /dev/null
-
 sudo chmod 644 $autologin_file
 sudo chown root:root $autologin_file
