c2e566cd59b906d23911b8f0f178b83ef7c309c3
Christian Smith <smith.christian.william@gmail.com>
2023-01-31 13:48:34 -0500

    don't log sway

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting
     if [ "$XDG_VTNR" -eq 1 ] && [ -z "$DISPLAY" ]
-        /usr/bin/sway 2>&1 >> ~/.sway.log
+        /usr/bin/sway 2>&1 > /dev/null
     end
 end
