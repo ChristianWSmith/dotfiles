cfff35421d42caa2b34904664124b49d93919fe9
Christian Smith <smith.christian.william@gmail.com>
2023-01-31 22:53:05 -0500

    fix .sys-settings.sh problem

--- .sys-settings.sh
+++ .sys-settings.sh
@@ -72,6 +72,8 @@ do
     if [ "$key" = "HOOKS" ]
     then
         new_line=$(echo $line | sed 's/udev/systemd fsck/g')
+        new_line=$(echo $new_line | sed 's/\(.*\)fsck\(.*\)fsck\(.*\)/\1fsck\2\3/g')
+        new_line=$(echo $new_line | sed 's/  / /g')
         echo $new_line >> .tmp
     else
         echo $line >> .tmp    
