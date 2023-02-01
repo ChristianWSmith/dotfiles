8efd5ed18d7ce11dcb101d6d04a0e4a0129a8d87
Christian Smith <smith.christian.william@gmail.com>
2023-01-31 19:41:01 -0500

    fix TERM var in autologin

--- .sys-settings.sh
+++ .sys-settings.sh
@@ -4,12 +4,11 @@ sudo usermod -a -G wheel $USER
 sudo usermod -a -G seat $USER
 sudo /bin/bash -c "rsync -a $HOME/.config/fish/ ~/.config/fish"
 
-# TODO: inject literal '$TERM' into file
 autologin_file="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
 sudo /bin/bash -c "rm -f $autologin_file; tee -a $autologin_file <<EOF
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin $USER %I $(which $TERM)
+ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin $USER %I \\\$TERM
 Environment=XDG_SESSION_TYPE=wayland
 EOF" > /dev/null
 sudo chmod 644 $autologin_file
