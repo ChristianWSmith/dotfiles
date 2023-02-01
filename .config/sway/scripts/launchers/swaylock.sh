dc4e084fa65cf4e9fe8a1de4ba95ff08daae8bef
Christian Smith <smith.christian.william@gmail.com>
2023-02-01 15:46:41 -0500

    make swaylock prioritize current swaybg image

--- .config/sway/scripts/launchers/swaylock.sh
+++ .config/sway/scripts/launchers/swaylock.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
 pkill -x swaylock
-backgrounds=(~/.config/sway/backgrounds/*)
 
-if [ "$backgrounds" != "$HOME/.config/sway/backgrounds/*" ]
+swaylock_command="swaylock -s fill -i"
+
+swaybg_file=""
+for ps_token in $(ps -ef | grep swaybg)
+do
+    if test -f $ps_token
+    then
+        swaybg_file=$ps_token
+        break
+    fi
+done
+
+if [ "$swaybg_file" != "" ]
 then
-	background=${backgrounds[$RANDOM%${#backgrounds[@]}]}
-	swaylock -s fill -i $background &
+    $swaylock_command $swaybg_file &
+else
+    backgrounds=(~/.config/sway/backgrounds/*)
+
+    if [ "$backgrounds" != "" ]
+    then
+        background=${backgrounds[$RANDOM%${#backgrounds[@]}]}
+        $swaylock_command $background &
+    else
+        swaylock &
+    fi
 fi
