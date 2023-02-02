7eaf564f7436d4188ba0e642b183280c9e827a00
Christian Smith <smith.christian.william@gmail.com>
2023-02-02 18:19:33 -0500

    add no steam animations to .sys-settings.sh

--- .sys-settings.sh
+++ .sys-settings.sh
@@ -154,3 +154,14 @@ do
     pip install $pip_package
 done
 # -PIP
+
+# +NO STEAM ANIMATIONS SKIN
+mkdir -p ~/.steam/root/skins
+cd ~/.steam/root/skins
+mkdir -p StopAnimations/resource
+cp -r ~/.steam/root/resource/styles StopAnimations/resource
+sed -i '/Notifications.SlideDirection/ s/"[A-Za-z]*"/"None"/' StopAnimations/resource/styles/* #no animation sliding
+sed -i '/Notifications.FadeInTime/ s/"0.[0-9+]*"/"0.0"/' StopAnimations/resource/styles/* #no fade in time for notifications
+sed -i '/Notifications.FadeOutTime/ s/"0.[0-9+]*"/"0.0"/' StopAnimations/resource/styles/* #no fade out time for notifications
+sed -i '/Notifications.StackSize/ s/"[0-9]*"/"5"/' StopAnimations/resource/styles/* #show max 5 notifications instead of max 3 at the same time
+# -NO STEAM ANIMATIONS SKIN
