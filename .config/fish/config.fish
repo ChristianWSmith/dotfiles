ac5027e523d5970dfc707cf75c835f3c8c746730
Christian Smith <smith.christian.william@gmail.com>
2023-02-02 08:13:48 -0500

    multi layout support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ if status is-interactive
     fish_add_path ~/.local/bin
     alias "update-package-list" "yay -Qe | cut -d' ' -f1 > ~/.packages"
     alias "update-etc-environment" "cat /etc/environment > ~/.etc-environment"
+    alias "update-pip-list" "pip list | tail -n +3 | cut -d' ' -f1 > ~/.pip-list"
     alias "shutdown" "systemctl poweroff --no-wall"
     alias "reboot" "systemctl reboot --no-wall --reboot"
     alias "baseline" "sudo timeshift --delete-all; sudo timeshift --create --comments 'base'"
--- .config/sway/scripts/launchers/master_stack_layout.sh
+++ .config/sway/scripts/launchers/master_stack_layout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ~/.config/sway/scripts/helpers/kill_layouts.sh
-~/.config/sway/scripts/layouts/alternating_layout.py &
+~/.config/sway/scripts/layouts/master_stack_layout.py &
