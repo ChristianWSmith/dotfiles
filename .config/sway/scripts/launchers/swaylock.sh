ec0d814a41fffa4e2d0970f8fcc0ba35d1ec0cdf
Christian Smith <smith.christian.william@gmail.com>
2023-02-01 19:50:17 -0500

    customized swaylock launcher

--- .config/sway/scripts/launchers/swaylock.sh
+++ .config/sway/scripts/launchers/swaylock.sh
@@ -2,7 +2,50 @@
 
 pkill -x swaylock
 
-swaylock_command="swaylock -s fill -i"
+blue="#3584e4"
+red="#c01c28"
+green="#26a269"
+yellow="#cd9309"
+gray="#242424"
+black="#000000"
+white="#ffffff"
+transparent="#00000000"
+
+swaylock_command="swaylock "
+swaylock_command="$swaylock_command --color $gray" #                  Turn the screen into the given color instead of white.
+swaylock_command="$swaylock_command --ignore-empty-password" #           When an empty password is provided, do not validate it.
+swaylock_command="$swaylock_command --show-failed-attempts" #            Show current count of failed authentication attempts.
+swaylock_command="$swaylock_command --daemonize" #                       Detach from the controlling terminal after locking.
+swaylock_command="$swaylock_command --hide-keyboard-layout" #            Hide the current xkb layout while typing.
+swaylock_command="$swaylock_command --scaling fill" #                  Image scaling mode: stretch, fill, fit, center, tile, solid_color.
+swaylock_command="$swaylock_command --bs-hl-color $yellow" #            Sets the color of backspace highlight segments.
+swaylock_command="$swaylock_command --caps-lock-bs-hl-color $yellow" #  Sets the color of backspace highlight segments when Caps Lock is active.
+swaylock_command="$swaylock_command --caps-lock-key-hl-color $green" # Sets the color of the key press highlight segments when Caps Lock is active.
+swaylock_command="$swaylock_command --inside-color $transparent" #           Sets the color of the inside of the indicator.
+swaylock_command="$swaylock_command --inside-clear-color $transparent" #     Sets the color of the inside of the indicator when cleared.
+swaylock_command="$swaylock_command --inside-caps-lock-color $transparent" # Sets the color of the inside of the indicator when Caps Lock is active.
+swaylock_command="$swaylock_command --inside-ver-color $transparent" #       Sets the color of the inside of the indicator when verifying.
+swaylock_command="$swaylock_command --inside-wrong-color $transparent" #     Sets the color of the inside of the indicator when invalid.
+swaylock_command="$swaylock_command --key-hl-color $green" #           Sets the color of the key press highlight segments.
+swaylock_command="$swaylock_command --layout-bg-color $transparent" #        Sets the background color of the box containing the layout text.
+swaylock_command="$swaylock_command --layout-border-color $transparent" #    Sets the color of the border of the box containing the layout text.
+swaylock_command="$swaylock_command --layout-text-color $transparent" #      Sets the color of the layout text.
+swaylock_command="$swaylock_command --line-color $gray" #             Sets the color of the line between the inside and ring.
+swaylock_command="$swaylock_command --line-clear-color $gray" #       Sets the color of the line between the inside and ring when cleared.
+swaylock_command="$swaylock_command --line-caps-lock-color $gray" #   Sets the color of the line between the inside and ring when Caps Lock is active.
+swaylock_command="$swaylock_command --line-ver-color $gray" #         Sets the color of the line between the inside and ring when verifying.
+swaylock_command="$swaylock_command --line-wrong-color $gray" #       Sets the color of the line between the inside and ring when invalid.
+swaylock_command="$swaylock_command --ring-color $gray" #             Sets the color of the ring of the indicator.
+swaylock_command="$swaylock_command --ring-clear-color $blue" #       Sets the color of the ring of the indicator when cleared.
+swaylock_command="$swaylock_command --ring-caps-lock-color $transparent" #   Sets the color of the ring of the indicator when Caps Lock is active.
+swaylock_command="$swaylock_command --ring-ver-color $blue" #         Sets the color of the ring of the indicator when verifying.
+swaylock_command="$swaylock_command --ring-wrong-color $red" #       Sets the color of the ring of the indicator when invalid.
+swaylock_command="$swaylock_command --separator-color $gray" #        Sets the color of the lines that separate highlight segments.
+swaylock_command="$swaylock_command --text-color $transparent" #             Sets the color of the text.
+swaylock_command="$swaylock_command --text-clear-color $transparent" #       Sets the color of the text when cleared.
+swaylock_command="$swaylock_command --text-caps-lock-color $transparent" #   Sets the color of the text when Caps Lock is active.
+swaylock_command="$swaylock_command --text-ver-color $transparent" #         Sets the color of the text when verifying.
+swaylock_command="$swaylock_command --text-wrong-color $transparent" #       Sets the color of the text when invalid.
 
 swaybg_file=""
 for ps_token in $(ps -ef | grep swaybg)
@@ -16,15 +59,16 @@ done
 
 if [ "$swaybg_file" != "" ]
 then
-    $swaylock_command $swaybg_file &
+    echo "HERE"
+    echo "$swaybg_file"
+    swaylock_command="$swaylock_command --image $swaybg_file"
 else
     backgrounds=(~/.config/sway/backgrounds/*)
-
     if [ "$backgrounds" != "" ]
     then
         background=${backgrounds[$RANDOM%${#backgrounds[@]}]}
-        $swaylock_command $background &
-    else
-        swaylock &
+        swaylock_command="$swaylock_command --image $background"
     fi
 fi
+
+$swaylock_command &
