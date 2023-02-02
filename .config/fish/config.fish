8c677f9bc37ee69a7a267f1cc3e59ff0a087cec5
Christian Smith <smith.christian.william@gmail.com>
2023-02-01 23:48:43 -0500

    add ~/.local/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     if [ "$XDG_VTNR" = "1" ] && [ -z "$DISPLAY" ]
         /usr/bin/sway 2>&1 > /dev/null
     end
+    fish_add_path ~/.local/bin
     alias "update-package-list" "yay -Qe | cut -d' ' -f1 > ~/.packages"
     alias "update-etc-environment" "cat /etc/environment > ~/.etc-environment"
     alias "shutdown" "systemctl poweroff --no-wall"
