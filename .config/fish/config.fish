07bffb5559e6003a30b3cf9d5b773dd6fcf27019
Christian Smith <smith.christian.william@gmail.com>
2023-02-04 09:46:19 -0500

    retry launching sway on tty1 (ugly fix for race condition on startup)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting
     if [ "$XDG_VTNR" = "1" ] && [ -z "$DISPLAY" ]
-        /usr/bin/sway 2>&1 > /dev/null
+        while ! /usr/bin/sway 2>&1 > /dev/null; :; end
     end
     fish_add_path ~/.local/bin
     alias "update-package-list" "yay -Qe | cut -d' ' -f1 > ~/.packages"
