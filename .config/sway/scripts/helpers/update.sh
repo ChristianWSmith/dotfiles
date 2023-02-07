fda251e2d0d4a162e196abaad23c9544c424cf35
Christian Smith <smith.christian.william@gmail.com>
2023-02-07 16:32:24 -0500

    cleaner update script

--- .config/sway/scripts/helpers/update.sh
+++ .config/sway/scripts/helpers/update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 echo "Updating..."
-yay -Syu --needed --norebuild --noredownload --nocleanmenu --nodiffmenu --noremovemake
+if yay -Syu --needed --norebuild --noredownload --nocleanmenu --nodiffmenu --noremovemake
+then
+    notify-send -t 5000 "Update complete."
+    text=""
+    tooltip="Up to date."
+else
+    notify-send --urgency=critical -t 5000 "Update failed."
+    text="⚠"
+    tooltip="Update failed."
+fi
+echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}" > ~/.update_reminder
 read -p "Press any key to continue..."
