4d2c8e3e1db1d6d17d3a45b6c9dba937b0b546ac
Christian Smith <smith.christian.william@gmail.com>
2023-02-08 10:27:32 -0500

    remove infinite loop from update reminder (we're cronning it now)

--- .config/sway/scripts/helpers/update_reminder.sh
+++ .config/sway/scripts/helpers/update_reminder.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 touch ~/.update_reminder
-while true
-do
-    if [ "$(checkupdates)" ] || [ "$(checkupdates-aur)" ]
-    then
-        notify-send -t 5000 "Updates required."
-        text=""
-        tooltip="Updates required."
-    else
-        text=""
-        tooltip="Up to date."
-    fi
-    echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}" > ~/.update_reminder
-done
+if [ "$(checkupdates)" ] || [ "$(checkupdates-aur)" ]
+then
+    notify-send -t 5000 "Updates required."
+    text=""
+    tooltip="Updates required."
+else
+    text=""
+    tooltip="Up to date."
+fi
+echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}" > ~/.update_reminder
+
