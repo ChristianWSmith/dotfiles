f75ad08f2fb32258ca95f58244f1f2f1ecdff99f
Christian Smith <smith.christian.william@gmail.com>
2023-02-09 07:21:58 -0500

    fix remove-bg script

--- .config/sway/scripts/helpers/remove_bg.sh
+++ .config/sway/scripts/helpers/remove_bg.sh
@@ -3,6 +3,6 @@
 current_bg=$($HOME/.config/sway/scripts/helpers/current_swaybg.sh)
 file_name=$(basename $current_bg)
 rm $current_bg
-sed -i "/$file_name/d" ~/.wallpapers.sh
+sed -i "/$file_name/d" ~/.assets/wallpapers.sh
 ~/.config/sway/scripts/launchers/swaybg.sh > /dev/null 2>&1
 
