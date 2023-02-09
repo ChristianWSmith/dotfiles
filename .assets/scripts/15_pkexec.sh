fb5ccbb1d3a9ddbf4cc0dd9e8e58c34c6199fb40
Christian Smith <smith.christian.william@gmail.com>
2023-02-09 08:12:48 -0500

    MORE cleanup

--- .assets/scripts/15_pkexec.sh
+++ .assets/scripts/15_pkexec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-~/.assets/pkexec-mask/pkexec-mask
-sudo cp ~/.assets/pkexec-mask/pkexec-mask /usr/bin/pkexec-mask
+~/.assets/bin/pkexec-mask
+sudo cp ~/.assets/bin/pkexec-mask /usr/bin/pkexec-mask
 sudo chmod --reference=$(which pkexec) /usr/bin/pkexec-mask
 sudo chown --reference=$(which pkexec) /usr/bin/pkexec-mask
 sudo mkdir -p /etc/pacman.d/hooks
