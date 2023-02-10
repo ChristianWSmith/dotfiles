4026c0c120fa3d42ae1cca19d4cc5ea5f6694f8b
Christian Smith <smith.christian.william@gmail.com>
2023-02-09 20:04:21 -0500

    update reminder waits until connected to make a call

--- .config/sway/scripts/helpers/update_reminder.sh
+++ .config/sway/scripts/helpers/update_reminder.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 touch ~/.update_reminder
+
+while ! ping -c 1 archlinux.org; do :; done
+
 if [ "$(checkupdates)" ] || [ "$(checkupdates-aur)" ]
 then
     notify-send -t 5000 "Updates required."
