f970c60cb92b29de5d6045fee67c193f35d81aa5
Christian Smith <smith.christian.william@gmail.com>
2023-02-10 17:21:46 -0500

    update

--- .assets/scripts/02_install_packages.sh
+++ .assets/scripts/02_install_packages.sh
@@ -3,7 +3,10 @@ set -e
 
 yay -S --needed $(cat ~/.assets/lists/package-list)
 
-yay -Qe > tmp
+yay -Qe | cut -d' ' -f1 > tmp
 remove=$(diff ~/.assets/lists/package-list tmp | grep '^>' | sed 's/^>\ //')
-yay -R $(remove)
+if [ "$remove" ]
+then
+    yay -R $remove
+fi
 rm tmp
--- .assets/scripts/17_pip.sh
+++ .assets/scripts/17_pip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 for pip_package in $(cat ~/.assets/lists/pip-list)
 do
