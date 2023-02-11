9a71418050b8f126053880b6f929a2bd8d95e04a
Christian Smith <smith.christian.william@gmail.com>
2023-02-10 22:09:59 -0500

    remove unneeded pip packages

--- .assets/scripts/17_pip.sh
+++ .assets/scripts/17_pip.sh
@@ -5,3 +5,10 @@ for pip_package in $(cat ~/.assets/lists/pip-list)
 do
     pip install $pip_package
 done
+
+pip list | cut -d' ' -f1 > tmp
+remove=$(diff tmp ~/.assets/lists/pip-list | grep '^>' | sed 's/^>\ //')
+for pip_package in ${remove[@]}
+do
+    pip uninstall $pip_package
+done
