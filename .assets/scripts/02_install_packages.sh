18f88091fef20778d469e43473e7912819363846
Christian Smith <smith.christian.william@gmail.com>
2023-02-10 22:08:02 -0500

    remove unneeded packages

--- .assets/scripts/02_install_packages.sh
+++ .assets/scripts/02_install_packages.sh
@@ -2,3 +2,8 @@
 set -e
 
 yay -S --needed $(cat ~/.assets/lists/package-list)
+
+yay -Qe | cut -d' ' -f1 > tmp
+remove=$(diff tmp ~/.assets/lists/package-list | grep '^>' | sed 's/^>\ //')
+yay -R $remove
+rm tmp
