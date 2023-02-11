022b5adf5f234f8166618e7d2061378e0e1b82a8
Christian Smith <smith.christian.william@gmail.com>
2023-02-10 22:32:20 -0500

    screenshot notifications cleanup

--- .config/scripts/launchers/screenshot.sh
+++ .config/scripts/launchers/screenshot.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/fish
 
-grim -g $(slurp -d) - | wl-copy
-~/.config/scripts/helpers/notify_clipboard.sh
+set dimensions "$(slurp -d)"
+
+if [ "$dimensions" ]
+    grim -g $dimensions - | wl-copy
+    notify-send -t 5000 "Screenshot copied to clipboard."
+else
+    notify-send -t 5000 "Screenshot canceled."
+end
