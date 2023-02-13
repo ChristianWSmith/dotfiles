b215f35880776b5e5b3914220a9c64a9ae0d846a
Christian Smith <smith.christian.william@gmail.com>
2023-02-13 09:53:03 -0500

    WIP master-stack

---
+++ .config/scripts/layouts/master_stack/common.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+MASTER_PREFIX="master-"
+STACK_PREFIX="stack-"
--- .config/scripts/layouts/master_stack/layout.py
+++ .config/scripts/layouts/master_stack/layout.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 import asyncio
-
 from functools import partial
-
 from i3ipc import Event
 from i3ipc.aio import Connection
+from common import MASTER_PREFIX, STACK_PREFIX
+import subprocess
 
 
-async def enforce_layout_workspace(workspace):
+async def enforce_layout_workspace(workspace, sway):
     tiled_windows = [leaf for leaf in workspace.leaves() if not leaf.type == "floating_con"]
+    master_mark = MASTER_PREFIX + workspace.name
+    stack_mark = STACK_PREFIX + workspace.name
     if len(tiled_windows) == 1:
         if workspace.nodes[0] != tiled_windows[0]:
             await tiled_windows[0].command("move up")
@@ -18,19 +20,33 @@ async def enforce_layout_workspace(workspace):
         left, right = tiled_windows
         await left.command("splitv")
         await right.command("splitv")
+        if master_mark not in left.parent.marks or stack_mark not in right.parent.marks:
+            tree = await sway.get_tree()
+            left = tree.find_by_id(left.id)
+            right = tree.find_by_id(right.id)
+            await left.parent.command(f"mark --add {master_mark}")
+            await right.parent.command(f"mark --add {stack_mark}")
 
 
-async def enforce_layout(head):
+async def enforce_layout(head, sway):
     if head.type == "workspace":
-        await enforce_layout_workspace(head)
+        await enforce_layout_workspace(head, sway)
     else:
         for node in head.nodes:
-            await enforce_layout(node)
+            await enforce_layout(node, sway)
+
+
+def dump(head, prefix="-"):
+    print(prefix, head.type, head.name, head.marks)
+    for node in head.nodes:
+        dump(node, prefix + "-")
 
 
 async def trigger(sway, _) -> None:
     tree = await sway.get_tree()
-    await enforce_layout(tree)
+    await enforce_layout(tree, sway)
+    # subprocess.run(["clear"])
+    # dump(await sway.get_tree())
 
 
 async def amain():
