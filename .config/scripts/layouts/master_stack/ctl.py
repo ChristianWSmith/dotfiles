e030912ef24d7c8293252f2f8349c5059b12161b
Christian Smith <smith.christian.william@gmail.com>
2023-02-13 12:49:41 -0500

    implement kill in master_stack/ctl.py

--- .config/scripts/layouts/master_stack/ctl.py
+++ .config/scripts/layouts/master_stack/ctl.py
@@ -29,12 +29,15 @@ directions = {
 }
 
 
+# HELPERS
+
 async def promote_pre(workspace, master_mark, stack_mark):
     for node in workspace.nodes:
         if stack_mark in node.marks:
             await node.nodes[0].command(f"mark --add {TEMP_MASTER_MARK}")
             await node.nodes[0].command(f"move container to mark {master_mark}")
-            break
+            return True
+    return False
 
 
 async def promote_post(workspace, master_mark):
@@ -45,7 +48,37 @@ async def promote_post(workspace, master_mark):
             break
 
 
+async def move_to_workspace_impl(tree, from_workspace, to_workspace, focus):
+    from_master_mark = MASTER_PREFIX + from_workspace.name
+    to_stack_mark = STACK_PREFIX + to_workspace.name
+
+    promoted = False
+    if from_master_mark in focus.parent.marks:
+        from_stack_mark = STACK_PREFIX + from_workspace.name
+        promoted = await promote_pre(from_workspace, from_master_mark, from_stack_mark)
+
+    move_to_stack_mark = False
+    for node in to_workspace.nodes:
+        if to_stack_mark in node.marks:
+            move_to_stack_mark = True
+            break
+
+    if move_to_stack_mark:
+        await focus.command(f"move container to mark {to_stack_mark}")
+    else:
+        await focus.command(f"move container to workspace {to_workspace.name}")
+
+    if promoted:
+        await promote_post(from_workspace, from_master_mark)
+
+
+# COMMANDS
+
 async def move(sway):
+    if sys.argv[2] not in directions.keys():
+        print(f"Usage: {sys.argv[0]} move [{'|'.join(directions.keys())}]")
+        exit(1)
+
     forward = directions[sys.argv[1]]["f"]
     backward = directions[sys.argv[1]]["b"]
 
@@ -63,52 +96,42 @@ async def move(sway):
         await sway.command(f"swap container with mark _swap; focus {forward}; unmark _swap")
     else:
         await sway.command(f"focus {backward}; unmark _swap")
-        await move_to_workspace(sway, (pre_tree, pre_workspace, post_workspace, pre_focus))
+        await move_to_workspace_impl(pre_tree, pre_workspace, post_workspace, pre_focus)
 
-
-async def move_to_workspace(sway, tree_from_to_focus=None):
-    if tree_from_to_focus is None:
-        tree = await sway.get_tree()
-        focus = tree.find_focused()
-        from_workspace = focus.workspace()
-        to_workspace_name = sys.argv[2]
-        to_workspace = None
-        for workspace in tree.workspaces():
-            if workspace.name == to_workspace_name:
-                to_workspace = workspace
-                break
-        if to_workspace is None:
-            print("error")
-            exit(1)
-    else:
-        tree, from_workspace, to_workspace, focus = tree_workspace_focus
-        
-    from_master_mark = MASTER_PREFIX + from_workspace.name
-    to_stack_mark = STACK_PREFIX + to_workspace.name
-
-    promoted = False
-    if from_master_mark in focus.parent.marks:
-        from_stack_mark = STACK_PREFIX + from_workspace.name
-        await promote_pre(from_workspace, from_master_mark, from_stack_mark)
-        promoted = True
-
-    move_to_stack_mark = False
-    for node in to_workspace.nodes:
-        if to_stack_mark in node.marks:
-            move_to_stack_mark = True
+async def move_to_workspace(sway):
+    if len(sys.argv) < 3:
+        print("Usage: {sys.argv[0]} move_to_workspace [workspace_name]")
+        exit(1) 
+    to_workspace_name = sys.argv[2]
+    tree = await sway.get_tree()
+    focus = tree.find_focused()
+    from_workspace = focus.workspace()
+    to_workspace = None
+    for workspace in tree.workspaces():
+        if workspace.name == to_workspace_name:
+            to_workspace = workspace
             break
-
-    if move_to_stack_mark:
-        await focus.command(f"move container to mark {to_stack_mark}")
-    else:
-        await focus.command(f"move container to workspace {to_workspace.name}")
-
-    if promoted:
-        await promote_post(from_workspace, from_master_mark)
+    if to_workspace is None:
+        print("Error: Workspace \"{to_workspace_name}\" not found.")
+        exit(1)
+    await move_to_workspace_impl(tree, from_workspace, to_workspace, focus)
 
 
 async def kill(sway):
-    pass
+    tree = await sway.get_tree()
+    focused = tree.find_focused()
+    workspace = focused.workspace()
+    master_mark = MASTER_PREFIX + workspace.name
+
+    promoted = False
+    if master_mark in focused.parent.marks:
+        stack_mark = STACK_PREFIX + workspace.name
+        promoted = await promote_pre(workspace, master_mark, stack_mark)
+
+    await focused.command("kill")
+
+    if promoted:
+        await promote_post(workspace, master_mark)
 
 
 async def amain():
