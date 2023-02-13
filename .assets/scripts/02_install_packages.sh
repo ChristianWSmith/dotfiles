6ce4c76a47c8c48d6e30e2e56310efccb628b8cb
Christian Smith <smith.christian.william@gmail.com>
2023-02-13 09:13:24 -0500

    only try to remove packages if there are packages to remove

--- .assets/scripts/02_install_packages.sh
+++ .assets/scripts/02_install_packages.sh
@@ -5,5 +5,8 @@ yay -S --needed $(cat ~/.assets/lists/package-list)
 
 yay -Qe | cut -d' ' -f1 > tmp
 remove=$(diff tmp ~/.assets/lists/package-list | grep '^>' | sed 's/^>\ //')
-yay -R $remove
+if [ "$remove" ]
+then
+    yay -R $remove
+fi
 rm tmp
