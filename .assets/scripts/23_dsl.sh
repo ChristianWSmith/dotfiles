0afe8fd679e0bb7d62debee58ec5cf222fed5872
Christian Smith <smith.christian.william@gmail.com>
2023-02-17 08:21:28 -0500

    cache dsl repo and restart server on rebuild

--- .assets/scripts/23_dsl.sh
+++ .assets/scripts/23_dsl.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 rustup install stable
+mkdir -p ~/.assets/cache
+cd ~/.assets/cache
 git clone https://www.github.com/ChristianWSmith/dsl-rs
 cd dsl-rs
+git pull
 ./build_release.sh
+
+restart_server=0
+if pkill -x dsl-server
+then
+    restart_server=1
+fi
+
 cp target/release/server ~/.local/bin/dsl-server
 cp target/release/client ~/.local/bin/dsl-client
-cd ..
-rm -rf dsl-rs
+
+if [ $restart_server ]
+then
+    dsl-server > /dev/null 2>&1 & disown
+fi
