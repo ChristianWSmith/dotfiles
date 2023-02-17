0b859b1056bd3468259335012011a854b1b5afe3
Christian Smith <smith.christian.william@gmail.com>
2023-02-17 07:46:21 -0500

    update

--- .assets/scripts/17_pip.sh
+++ .assets/scripts/17_pip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 for pip_package in $(cat ~/.assets/lists/pip-list)
 do
@@ -7,8 +6,13 @@ do
 done
 
 pip list | cut -d' ' -f1 > tmp
-remove=$(diff tmp ~/.assets/lists/pip-list | grep '^>' | sed 's/^>\ //')
+remove=$(diff ~/.assets/lists/pip-list tmp | grep '^>' | sed 's/^>\ //' | tail -n+3)
+echo $remove
 for pip_package in ${remove[@]}
 do
-    pip uninstall $pip_package
+    if ! pip uninstall $pip_package
+    then
+        sudo pip uninstall $pip_package
+    fi
 done
+rm tmp
