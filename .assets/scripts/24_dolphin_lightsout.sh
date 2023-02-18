5b9c352986ccc539d288e0d2695ebc98b6fb893c
Christian Smith <smith.christian.william@gmail.com>
2023-02-18 00:14:55 -0500

    dolphin dark mode

---
+++ .assets/scripts/24_dolphin_lightsout.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -P ~/.local/share/dolphin-emu/Styles
+wget -O ~/.local/share/dolphin-emu/Styles/lightsout.qss "https://raw.githubusercontent.com/Humanoidear/Lightsout/main/Lightsout/Lightsout_V.2.0%20(dark).qss"
