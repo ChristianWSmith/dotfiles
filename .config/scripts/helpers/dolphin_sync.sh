7e037ff910ce439359c91fc2c95c449496370813
Christian Smith <smith.christian.william@gmail.com>
2023-02-21 13:49:34 -0500

    dolphin_sync.sh remove broken links

--- .config/scripts/helpers/dolphin_sync.sh
+++ .config/scripts/helpers/dolphin_sync.sh
@@ -18,3 +18,10 @@ for remote_file in $(cd "$REMOTE_DIR"; find . -type f | cut -d'/' -f2)
 do
     ln -sf "${REMOTE_DIR}/${remote_file}" "${LOCAL_DIR}/${remote_file}"
 done
+
+for link_file in $(cd "$LOCAL_DIR"; find . -type l | cut -d'/' -f2)
+do
+    if [ ! -e "$link_file" ] ; then
+        unlink "${LOCAL_DIR}/$link_file"
+    fi
+done
