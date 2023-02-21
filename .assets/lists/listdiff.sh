0f173193e36dd150843aa2054723b03d91782d2e
Christian Smith <smith.christian.william@gmail.com>
2023-02-21 10:51:23 -0500

    improve listdiff

--- .assets/lists/listdiff.sh
+++ .assets/lists/listdiff.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/fish
 
+cd ~/.assets/lists/
 yay -Qe | cut -d' ' -f1 > package-list-tmp
-pip list | cut -d' ' -f1 > pip-list-tmp
+pip list | cut -d' ' -f1 | tail -n+3 > pip-list-tmp
 
 figlet PACKAGES | lolcat
 diff package-list package-list-tmp
