5898585e41065ae2e3feb79604c6e24956e2d481
Christian Smith <smith.christian.william@gmail.com>
2023-02-24 11:10:27 -0500

    fix doas setup to not require doas ahead of time lol

--- .assets/scripts/26_doas.sh
+++ .assets/scripts/26_doas.sh
@@ -6,5 +6,14 @@ then
     exit 1
 fi
 
-doas /bin/bash -c "echo 'permit persist :wheel' > /etc/doas.conf"
-doas /bin/bash -c "ln -sf /usr/bin/doas /usr/bin/sudo"
+if test -f /etc/doas.conf > /dev/null 2>&1
+then
+    escalate="doas"
+else
+    escalate="sudo"
+fi
+
+$escalate /bin/bash -c "echo 'permit persist :wheel' > /etc/doas.conf"
+$escalate /bin/bash -c "ln -sf /usr/bin/doas /usr/bin/sudo"
+$escalate /bin/bash -c "chown -c root:root /etc/doas.conf"
+$escalate /bin/bash -c "chmod -c 0400 /etc/doas.conf"
