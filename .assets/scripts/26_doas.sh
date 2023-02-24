d178926d3d9e1da2111f2a7b453621b51875221f
Christian Smith <smith.christian.william@gmail.com>
2023-02-24 11:04:13 -0500

    doas setup script

--- .assets/scripts/26_doas.sh
+++ .assets/scripts/26_doas.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo /bin/bash -c "echo 'permit :wheel' > /etc/doas.conf"
+if ! which doas > /dev/null 2>&1
+then
+    "ERROR: opendoas not installed."
+    exit 1
+fi
+
+sudo /bin/bash -c "echo 'permit nopass :wheel' > /etc/doas.conf"
+sudo /bin/bash -c "ln -sf /usr/bin/doas /usr/bin/sudo"
