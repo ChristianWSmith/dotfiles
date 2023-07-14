a90790cc5ece5971d7474a904590860f49926c31
Christian Smith <smith.christian.william@gmail.com>
2023-07-14 17:25:42 -0400

    more robust xboxdrv restart

--- .config/scripts/helpers/hitbox.sh
+++ .config/scripts/helpers/hitbox.sh
@@ -8,9 +8,9 @@ then
 fi
 
 xboxdrv_keepalive () {
-    while ! xboxdrv --evdev /dev/input/${EVENT_ID} --evdev-keymap "BTN_TRIGGER=A,BTN_THUMB2=X,BTN_TOP=Y,BTN_THUMB=B,BTN_PINKIE=LB,BTN_TOP2=RB,BTN_BASE2=LT,BTN_BASE=RT,BTN_BASE5=TL,BTN_BASE6=TR,BTN_BASE3=Back,BTN_BASE4=Start" --evdev-absmap "ABS_X=x1,ABS_Y=y1" --axismap "-y1=y1" --mimic-xpad --silent
+    while [ -e "/dev/input/${EVENT_ID}" ]
     do
-        :
+        xboxdrv --evdev /dev/input/${EVENT_ID} --evdev-keymap "BTN_TRIGGER=A,BTN_THUMB2=X,BTN_TOP=Y,BTN_THUMB=B,BTN_PINKIE=LB,BTN_TOP2=RB,BTN_BASE2=LT,BTN_BASE=RT,BTN_BASE5=TL,BTN_BASE6=TR,BTN_BASE3=Back,BTN_BASE4=Start" --evdev-absmap "ABS_X=x1,ABS_Y=y1" --axismap "-y1=y1" --mimic-xpad --silent
     done
 }
 
