919af69921187b0168189a2edcaf3fffb72ce9d8
Christian Smith <smith.christian.william@gmail.com>
2023-07-30 07:47:09 -0400

    hyprland waybar

--- .config/scripts/launchers/waybar.sh
+++ .config/scripts/launchers/waybar.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 pkill -x waybar
-waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
 
+if [ "$1" = "sway" ]
+then
+    waybar -c ~/.config/waybar/config_sway -s ~/.config/waybar/style.css &
+elif [ "$1" = "hyprland" ]
+then
+    waybar -c ~/.config/waybar/config_hyprland -s ~/.config/waybar/style.css &
+fi
