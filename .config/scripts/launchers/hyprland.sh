0d435e435c963c60dbf23fabb420e4ab208bd151
Christian Smith <smith.christian.william@gmail.com>
2023-07-30 19:32:57 -0400

    remove pkexec foolery

--- .config/scripts/launchers/hyprland.sh
+++ .config/scripts/launchers/hyprland.sh
@@ -7,13 +7,14 @@ export GTK_THEME=Adw-dark
 export GTK_ICON_THEME=Papirus-Dark
 export EDITOR=vim
 export WEB_BROWSER=google-chrome-stable
-export GUI_FILE_BROWSER=Thunar
+export GUI_FILE_BROWSER=nemo
 export TUI_FILE_BROWSER=ranger
 export TERMINAL=footclient
 export TUI_TASK_MANAGER=btop
 export TUI_NETWORK_MANAGER=nmtui
 export MOZ_ENABLE_WAYLAND=1
-export XDG_CURRENT_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=hyprland
+export XDG_CONFIG_HOME=$HOME/.config
 export TUI_VOLUME_CONTROL=pulsemixer
 export GUI_VOLUME_CONTROL=pavucontrol
 
