6a2f7cc75c9f99efff4e2e8af1453746241e3598
Christian Smith <smith.christian.william@gmail.com>
2023-07-31 11:47:26 -0400

    fix waybar/hypr

--- .assets/scripts/12_mask_audit.sh
+++ .assets/scripts/12_mask_audit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
+sudo systemctl unmask systemd-journald-audit.socket
+sudo systemctl stop systemd-journald-audit.socket
+sudo systemctl disable systemd-journald-audit.socket
 sudo systemctl mask systemd-journald-audit.socket
