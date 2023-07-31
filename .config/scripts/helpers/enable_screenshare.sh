72ca68e023bbd51b0efb49646a16f52ad85f411b
Christian Smith <smith.christian.william@gmail.com>
2023-07-31 17:27:15 -0400

    fix xdg-desktop-portal-hyprland

--- .config/scripts/helpers/enable_screenshare.sh
+++ .config/scripts/helpers/enable_screenshare.sh
@@ -4,7 +4,9 @@ if [ "$1" = "sway" ]
 then
     systemctl --user unmask xdg-desktop-portal
     systemctl --user unmask xdg-desktop-portal-wlr
-    systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    dbus-update-activation-environment
+    dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
     systemctl --user start xdg-desktop-portal
     systemctl --user start xdg-desktop-portal-wlr
     systemctl --user mask xdg-desktop-portal
@@ -13,7 +15,9 @@ elif [ "$1" = "hyprland" ]
 then
     systemctl --user unmask xdg-desktop-portal
     systemctl --user unmask xdg-desktop-portal-hyprland
-    systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+    dbus-update-activation-environment
+    dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
     systemctl --user start xdg-desktop-portal
     systemctl --user start xdg-desktop-portal-hyprland
     systemctl --user mask xdg-desktop-portal
