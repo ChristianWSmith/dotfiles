1c5488667083bbb03b9b55ac89f931b29a6cd95b
Christian Smith <smith.christian.william@gmail.com>
2023-08-08 22:26:13 -0400

    power icons in notification

--- .config/scripts/helpers/power_profile.sh
+++ .config/scripts/helpers/power_profile.sh
@@ -9,21 +9,21 @@ do_update=1
 
 if [ "$1" = "$POWER_SAVER" ]
 then
-    notify-send -t 5000 "Power profile set to saver."
+    notify-send --icon=~/.local/share/icons/leaf.svg -t 5000 "Power profile set to saver."
     powerprofilesctl set $POWER_SAVER &
     text=""
     tooltip="Power Profile: Saver "
     internal=$POWER_SAVER
 elif [ "$1" = "$BALANCED" ]
 then
-    notify-send -t 5000 "Power profile set to balanced."
+    notify-send --icon=~/.local/share/icons/scale-balanced.svg -t 5000 "Power profile set to balanced."
     powerprofilesctl set $BALANCED &
     text=""
     tooltip="Power Profile: Balanced "
     internal=$BALANCED
 elif [ "$1" = "$PERFORMANCE" ]
 then
-    notify-send -t 5000 "Power profile set to performance."
+    notify-send --icon=~/.local/share/icons/bolt.svg -t 5000 "Power profile set to performance."
     powerprofilesctl set $PERFORMANCE &
     text=""
     tooltip="Power Profile: Performance "
@@ -32,6 +32,8 @@ else
     notify-send -t 5000 "Requested power profile not recognized."
     do_update=0
 fi
+
+tooltip="${tooltip}\nLeft Click: Saver\nMiddle Click: Balanced\nRight Click: Performance"
 
 if [ $do_update -eq 1 ]
 then
