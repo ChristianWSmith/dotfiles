7b3e1eb105ffedc22018001973d2499167cc18da
Christian Smith <smith.christian.william@gmail.com>
2023-08-10 11:54:28 -0400

    clean up power profile script

--- .config/scripts/helpers/power_profile.sh
+++ .config/scripts/helpers/power_profile.sh
@@ -9,22 +9,19 @@ do_update=1
 
 if [ "$1" = "$POWER_SAVER" ]
 then
-    notify-send --icon=power-profile-power-saver-symbolic -t 5000 "Power profile set to saver."
-    powerprofilesctl set $POWER_SAVER &
+    icon=power-profile-power-saver-symbolic
     text=""
     tooltip="Power Profile: Saver "
     internal=$POWER_SAVER
 elif [ "$1" = "$BALANCED" ]
 then
-    notify-send --icon=power-profile-balanced-symbolic -t 5000 "Power profile set to balanced."
-    powerprofilesctl set $BALANCED &
+    icon=power-profile-balanced-symbolic
     text=""
     tooltip="Power Profile: Balanced "
     internal=$BALANCED
 elif [ "$1" = "$PERFORMANCE" ]
 then
-    notify-send --icon=power-profile-performance-symbolic -t 5000 "Power profile set to performance."
-    powerprofilesctl set $PERFORMANCE &
+    icon=power-profile-performance-symbolic
     text=""
     tooltip="Power Profile: Performance "
     internal=$PERFORMANCE
@@ -33,9 +30,15 @@ else
     do_update=0
 fi
 
-tooltip="${tooltip}\nLeft Click: Saver\nMiddle Click: Balanced\nRight Click: Performance"
 
 if [ $do_update -eq 1 ]
 then
-    echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"internal\": \"$internal\"}" > $POWER_PROFILE
+    if powerprofilesctl set $internal
+    then
+        notify-send --icon=$icon -t 5000 "Set $tooltip"
+        tooltip="${tooltip}\nLeft Click: Saver\nMiddle Click: Balanced\nRight Click: Performance"
+        echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}" > $POWER_PROFILE
+    else
+        notify-send --urgency="critical" -t 5000 "Failed to set $tooltip"
+    fi
 fi
