4980ec16bd8ccf858f5c3b36cc609341704c0c7c
Christian Smith <smith.christian.william@gmail.com>
2023-08-10 11:09:14 -0400

    clean up assets

--- .assets/funky_scripts/25_setup_google_drive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! rclone listremotes | grep GoogleDrive > /dev/null 2>&1
-then
-    mkdir -p ~/GoogleDrive
-    rclone config create GoogleDrive drive
-    ~/.config/scripts/helpers/mount_google_drive.sh
-fi
