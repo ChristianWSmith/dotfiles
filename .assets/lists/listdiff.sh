d4d2f569d5e2460b5216a5040e2ea19ebee525f9
Christian Smith <smith.christian.william@gmail.com>
2023-08-17 19:18:03 -0400

    this is getting a bit much

--- .assets/lists/listdiff.sh
+++ .assets/lists/listdiff.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/fish
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 cd ~/.assets/lists/
-yay -Qe | cut -d' ' -f1 > package-list-tmp
-pip list | cut -d' ' -f1 | tail -n+3 > pip-list-tmp
+yay -Qe | cut -d' ' -f1 | sort > package-list-tmp
 
 figlet PACKAGES | lolcat
-diff package-list package-list-tmp
-figlet PIP | lolcat
-diff pip-list pip-list-tmp
 
-rm package-list-tmp pip-list-tmp
+echo -e "$GREEN"
+comm -13 package-list package-list-tmp | sed 's/^/+ /g'
+
+echo -e "$RED"
+comm -23 package-list package-list-tmp | sed 's/^/- /g'
+
+echo -e "$NC"
+rm package-list-tmp
